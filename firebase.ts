4f12f9bee2bb5f02c2a002fd1068fb95a2aa01bd
thebase <73353819+thebase666@users.noreply.github.com>
2026-01-02 19:36:17 +0800

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -9,12 +9,6 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA6XTrXrZz1axWm35sYYQL39GxU7Im1eZ0",
-  authDomain: "expo2-3100a.firebaseapp.com",
-  projectId: "expo2-3100a",
-  storageBucket: "expo2-3100a.firebasestorage.app",
-  messagingSenderId: "529345093106",
-  appId: "1:529345093106:web:980792d073f0c7e7a65f6b",
 };
 
 // Initialize Firebase
